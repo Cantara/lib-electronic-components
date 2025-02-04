e3690636e7a012b566bf9824208428d41426be04
totto <totto@totto.org>
2025-02-04 08:54:30 +0100

    added module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,19 @@ module lib.electronic.components {
     // Require other modules (if any). For example, if you need SLF4J:
     requires org.slf4j;
 
+    // Add opens directives for Jackson
+    opens no.cantara.electronic.component to com.fasterxml.jackson.databind;
+
+    // If you have subpackages that also need to be serialized, add them too
+    opens no.cantara.electronic.component.advanced to com.fasterxml.jackson.databind;
+    opens no.cantara.electronic.component.lib to com.fasterxml.jackson.databind;
+
+
     // If you have transitive dependencies, you can declare them as transitive.
     // For example, if clients of your module should also see Jackson:
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.annotation;
+    requires transitive com.fasterxml.jackson.databind;
+
 }
 
