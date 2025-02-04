61ad8259326d0acf285d88efe02670f59752348e
totto <totto@totto.org>
2025-02-04 09:03:52 +0100

    fixing module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,27 +1,25 @@
 module lib.electronic.components {
-    // Export your public packages
+    // Export your packages
     exports no.cantara.electronic.component;
     exports no.cantara.electronic.component.advanced;
     exports no.cantara.electronic.component.lib;
-    exports no.cantara.electronic.component.lib.manufacturers;
-    exports no.cantara.electronic.component.lib.connectors;
     exports no.cantara.electronic.component.lib.componentsimilaritycalculators;
-    // Require other modules (if any). For example, if you need SLF4J:
-    requires org.slf4j;
+    exports no.cantara.electronic.component.lib.connectors;
+    exports no.cantara.electronic.component.lib.manufacturers;
 
     // Add opens directives for Jackson
     opens no.cantara.electronic.component to com.fasterxml.jackson.databind;
-
-    // If you have subpackages that also need to be serialized, add them too
     opens no.cantara.electronic.component.advanced to com.fasterxml.jackson.databind;
     opens no.cantara.electronic.component.lib to com.fasterxml.jackson.databind;
+    opens no.cantara.electronic.component.lib.componentsimilaritycalculators to com.fasterxml.jackson.databind;
+    opens no.cantara.electronic.component.lib.connectors to com.fasterxml.jackson.databind;
+    opens no.cantara.electronic.component.lib.manufacturers to com.fasterxml.jackson.databind;
 
+    // Required dependencies
+    requires org.slf4j;
 
-    // If you have transitive dependencies, you can declare them as transitive.
-    // For example, if clients of your module should also see Jackson:
+    // Transitive dependencies that should be visible to consumers of this module
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.annotation;
     requires transitive com.fasterxml.jackson.databind;
-
 }
-
